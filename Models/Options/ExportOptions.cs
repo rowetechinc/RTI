148048f45b5c1006c3cbd325db0d2e8495fe9424
Rico Castelo <rcastelo@rowetechinc.com>
2017-10-06 14:06:49 -0700

    Updated export options with screening options.

--- Models/Options/ExportOptions.cs
+++ Models/Options/ExportOptions.cs
@@ -1,34 +1,39 @@
 ﻿/*
- * Copyright © 2011 
- * Rowe Technology Inc.
+ * Copyright 2011, Rowe Technology Inc. 
  * All rights reserved.
  * http://www.rowetechinc.com
+ * https://github.com/rowetechinc
  * 
- * Redistribution and use in source and binary forms, with or without
- * modification is NOT permitted.
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
- * POSSIBILITY OF SUCH DAMAGE.
+ *  1. Redistributions of source code must retain the above copyright notice, this list of
+ *      conditions and the following disclaimer.
+ *      
+ *  2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *      of conditions and the following disclaimer in the documentation and/or other materials
+ *      provided with the distribution.
+ *      
+ *  THIS SOFTWARE IS PROVIDED BY Rowe Technology Inc. ''AS IS'' AND ANY EXPRESS OR IMPLIED 
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Rowe Technology Inc.
  * 
  * HISTORY
  * -----------------------------------------------------------------
  * Date            Initials    Version    Comments
  * -----------------------------------------------------------------
- * 03/31/2014      RC          2.21.4       Initial coding
- * 07/16/2014      RC          2.23.0       Added SetMaxBin() set all the max bin values.
- * 10/31/2014      RC          3.0.2        Added IsRangeTrackingDataSetOn.
- * 11/03/2014      RC          3.0.2        Added PressureOffset and CorrelationThreshold.
- * 09/28/2016      RC          3.3.2      Added export of Velocity Vectors in CSV.
+ * 07/23/2017      RC          1.0.0      Initial coding
+ * 10/06/2017      RC          1.1.0      Added screening options.
  * 
  */
 
@@ -189,6 +194,21 @@ namespace RTI
         /// Turn on or off the NMEA 2 dataset.
         /// </summary>
         public bool IsNmea2DataSetOn { get; set; }
+
+        /// <summary>
+        /// Turn on or off retransforming the data.
+        /// </summary>
+        public bool IsRetransformData { get; set; }
+
+        /// <summary>
+        /// Turn on or off marking bad below bottom.
+        /// </summary>
+        public bool IsMarkBadBelowBottom { get; set; }
+
+        /// <summary>
+        /// Turn on or off removing the ship speed.
+        /// </summary>
+        public bool IsRemoveShipSpeed { get; set; }
 
         #region Waves
 
@@ -351,6 +371,9 @@ namespace RTI
             GoodEarthMaxBin = 0;
             CorrelationThreshold = 0.0f;
             PressureOffset = 0.0f;
+            IsRetransformData = false;
+            IsMarkBadBelowBottom = true;
+            IsRemoveShipSpeed = true;
         }
 
         /// <summary>
